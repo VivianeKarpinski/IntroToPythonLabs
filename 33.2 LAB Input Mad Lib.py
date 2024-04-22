633e52c5211b281c2b2983056dfdbcba087e5afb
Viviane Karpinski <129092348+VivianeKarpinski@users.noreply.github.com>
2024-04-22 19:17:20 -0400

    Add files via upload

---
+++ 33.2 LAB Input Mad Lib.py
@@ -0,0 +1,20 @@
+"""
+33.2 LAB: Input Mad Lib
+
+Mad libs are activities that have a person provide various words, which are used to construct a short story
+in unexpected (and hopefully funny ways).
+
+Complete a program that reads four values in variables first_name, generic_location, whole_number, and plural_noun.
+The program uses the input values to output a short story.
+
+"""
+# Read a value from a user and store the value in first_name
+first_name = input()
+
+# TODO: Type your code to read three more values here.
+generic_location = input()
+whole_number = input()
+plural_noun = input()
+
+# Output a short story using the four input values. Do not modify the code below.
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
