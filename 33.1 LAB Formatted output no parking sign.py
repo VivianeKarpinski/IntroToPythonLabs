47b6d20d1b79abd95ee02f695e68311c7e6102fc
Viviane Karpinski <129092348+VivianeKarpinski@users.noreply.github.com>
2024-04-22 19:13:19 -0400

    Add files via upload

---
+++ 33.1 LAB Formatted output no parking sign.py
@@ -0,0 +1,12 @@
+"""
+33.1 LAB: Formatted Output: No parking sign
+
+Write a program that prints a formatted "No parking" sign. Note that the first line has two leading spaces.
+For all labs end with a new line (unless stated otherwise)
+
+Ex output:
+  NO PARKING
+2:00 - 6:00 a.m.
+"""
+
+print('  NO PARKING\n2:00 - 6:00 a.m.')
