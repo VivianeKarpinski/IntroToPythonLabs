e55c4b745fb08a0855044b7b5f0f7861b7f31d73
Viviane Karpinski <129092348+VivianeKarpinski@users.noreply.github.com>
2024-04-22 19:43:54 -0400

    Add files via upload

---
+++ 33.7 LAB Smallest number.py
@@ -0,0 +1,19 @@
+"""
+33.7 LAB: Smallest number
+
+Write a program whose inputs are three integers, and whose output is the smallest of three values
+
+If input is:
+7
+15
+3
+
+The output is 
+3
+
+"""
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+
+print(min(num_1, num_2, num_3))
